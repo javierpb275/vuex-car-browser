3bb19d12372506f2cdb1456a4ccf2b0f92c7a73b
javierpb275 <javierpb275@gmail.com>
2021-04-18 17:43:21 +0200

    defined logic of vuexsearchlist

--- src/components/VuexSearchList.vue
+++ src/components/VuexSearchList.vue
@@ -4,6 +4,32 @@
     </div>
 </template>
 
+<script>
+    import {mapGetters} from 'vuex'
+
+    export default {
+      data () {
+        return {
+          numberOfColumns: 3
+        }
+      },
+      computed: {
+        ...mapGetters(['filteredCars'])
+      },
+      methods: {
+        available (bool) {
+          return bool ? 'Yes' : 'No'
+        } ,
+        carModel (carModel) {
+          return `Model: ${carModel}`
+        },
+        blockRandom () {
+          return `block-${Math.floor(Math.random() * 6) + 1}`//return random number between 1 - 6. class block
+        }
+      }
+    }
+</script>
+
 <style lang="stylus">
  @import "../assets/search.styl"; 
 </style>
