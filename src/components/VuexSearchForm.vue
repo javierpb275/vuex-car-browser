7530b667ad9310d3f1324994dd1147ae0c7501d7
javierpb275 <javierpb275@gmail.com>
2021-04-18 13:36:01 +0200

    created vuexsearchform component. imported datepicker and its languages and as component and defined data with options for avaible property and added languages and selected english by default.

---
+++ src/components/VuexSearchForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        test
+    </div>
+</template>
+
+<script>
+//datepicker:
+import DatePicker from 'vuejs-datepicker';
+import * as langs from 'vuejs-datepicker/src/locale';
+
+export default {
+    components: {
+        DatePicker
+    },
+    data() {
+        return {
+            options: [
+                //selection field (for available property of filter (store))
+                { text: 'Yes', value: true},
+                { text: 'No', value: false}
+            ],
+            //language by default is English
+            languages: langs,
+            lang: 'en'
+        }
+    }
+}
+</script>
